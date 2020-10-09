1c149e03327adcc9c39512d7375425ea110b89ea
unknown <merabhai786786@gmail.com>
2020-10-09 09:39:47 -0700

    creating the email and phone number feild

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -46,6 +46,12 @@ const Heading = () => {
    
           <input name="lname" onChange={inputEventTwo}
             style={{ width: "50%", border: "none", textAlign: "center", outline: "none", margin: "20px 0", padding: "10px 20px" }} type="text" placeholder="Enter Your Last Name" value={fullname.lname} />
+            
+          <input name="email" onChange={inputEventTwo}
+            style={{ width: "50%", border: "none", textAlign: "center", outline: "none", margin: "20px 0", padding: "10px 20px" }} type="email" placeholder="Enter Your Email" value={fullname.email} />
+            
+          <input name="phone" onChange={inputEventTwo}
+            style={{ width: "50%", border: "none", textAlign: "center", outline: "none", margin: "20px 0", padding: "10px 20px" }} type="number" placeholder="Enter Your Mobile Number" value={fullname.phone} />
 
           <button style={{ padding: "0.1px 0.5rem", fontSize: "2rem", cursor: "pointer", background: "deepskyblue", border: "3px solid wheat", borderRadius: "4px", outline: "none" }}>Clik Me 👍</button>
         </div>
